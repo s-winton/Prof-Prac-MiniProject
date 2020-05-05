430f806e9d1efb902694113c4b876142e1114399
Your Name <you@example.com>
2020-05-05 16:18:18 +1200

    changes

---
+++ Mini_Group_Project/Mini_Group_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mini_Group_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
