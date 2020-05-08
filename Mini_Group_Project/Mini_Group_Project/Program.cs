1d2014bd65b1e2a5572e97076c811a4eeb496484
Your Name <you@example.com>
2020-05-08 13:34:37 +1200

    changes

--- Mini_Group_Project/Mini_Group_Project/Program.cs
+++ Mini_Group_Project/Mini_Group_Project/Program.cs
@@ -8,6 +8,24 @@ namespace Mini_Group_Project
         static void Main(string[] args)
         {
             Console.WriteLine("This is really hard make brain go ouch");
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("skdoajhgkoiajsdfgn");
+
+
+
+
         }
     }
 }
